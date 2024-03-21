5d34eaaa6f92093de89af6dcec5549b2c936fe48
Jared Pellegrini <jpellegrini@gmail.com>
2024-03-21 15:05:56 -0400

    v0.9: Added card deck
    There's still some cleanup to do, but it works :)

--- savageRoller.ino
+++ savageRoller.ino
@@ -4,14 +4,19 @@
  * @copyright Copyright 2023 Douglas P. Fields, Jr.
  * @license Apache License, Version 2.0 - https://www.apache.org/licenses/LICENSE-2.0
  * @brief   Rolls dice for the Savage Worlds game
- * @version 0.7
- * @date    2024-03-14 (Created 2023-10-30)
+ * @version 0.9
+ * @date    2024-03-21 (Created 2023-10-30)
  *
  * Targets the M5 Cardputer.
  *
+ * ROLLING:
  * If you roll more than one die, it adds them.
  * If you use a wild, it replaces the lowest rolled die if higher.
  * Each die can explode - adds another roll if it rolls the highest number.
+ *
+ * DECK OF CARDS:
+ * A card will only be drawn from the deck once per shuffle.
+ * Toggling jokers will force a re-shuffle.
  * 
  * NOTEs:
  * 1. String (Capital String) is an Arduino String not a C++ std::string
@@ -21,6 +26,7 @@
  *    when updating the display
  *
  * CHANGELOG
+ * v0.9: Added card deck
  * v0.8: Roll on pressing the return/enter key.
  * v0.7: Added CRITICAL FAIL message
  * v0.6: Added toggle for exploding dice (default=on)
@@ -40,9 +46,10 @@
 #include <vector>
 
 const uint8_t MAJOR_VERSION = 0;
-const uint8_t MINOR_VERSION = 8;
+const uint8_t MINOR_VERSION = 9;
+const uint8_t MAX_CARDS_TO_SHOW = 8;
 
-enum class Page { Splash, Roller };
+enum class Page { Splash, Roller, DeckofCards };
 Page currentPage = Page::Splash; // What UI page are we displaying?
 
 // User inputs
@@ -79,6 +86,12 @@ std::vector<long> prevRolls; // The last n previous rolls
 String rollDetails;
 String wildDetails;
 
+bool allowJokers = 1;
+uint8_t cardsToShow = 0;
+
+String suits[4] = {"Clubs", "Diamonds", "Hearts", "Spades"};
+std::vector<String> deck;
+std::vector<String> drawn;
 
 /* Main Arduino setup entry point.
  */
@@ -97,6 +110,7 @@ void setup() {
   // Initialize the random seed
   randomSeed(analogRead(0));
   prevRolls.reserve(ROLLS_TO_SAVE);
+  shuffleDeck();
 }
 
 /* Look at all the currently pressed keys,
@@ -146,7 +160,7 @@ bool isNewlyPressed(char c) {
 /*
  * If any keys are newly pressed, update state appropriately.
  */
-void handleKeys() {
+void rollerHandleKeys() {
   if (M5Cardputer.Keyboard.isChange()) {
     calculateNewlyPressed();
     if (isNewlyPressed('4')) {
@@ -179,7 +193,6 @@ void handleKeys() {
         stateChange = 1;
       }
     }
-
     // Remove dice
     if (isNewlyPressed('1')) {
       if (numD12 > 0) {
@@ -211,7 +224,6 @@ void handleKeys() {
         stateChange = 1;
       }
     }
-
     if (isNewlyPressed('w')) {
       includeWild = !includeWild;
       stateChange = 1;
@@ -317,6 +329,86 @@ long doRoll() {
   return retval;
 } // doRoll
 
+void shuffleDeck() {
+  //reset the number of cards shown
+  cardsToShow = 0;
+  //empty the drawn pile
+  drawn.clear();
+  //empty the deck
+  deck.clear();
+  // insert 2-10, then J, Q, K, A for each suit
+  for (uint8_t i = 0; i < 4; i++) {
+    for (uint8_t j = 2; j <= 10; j++) { deck.push_back(String(j) + "-" + suits[i]); }
+    deck.push_back("J-" + suits[i]);
+    deck.push_back("Q-" + suits[i]);
+    deck.push_back("K-" + suits[i]);
+    deck.push_back("A-" + suits[i]);
+  }
+  // add the two jokers if allowed
+  if (allowJokers) {
+    deck.push_back("Joker-RED");
+    deck.push_back("Joker-BLACK");
+  }
+}
+
+void displayDeck() {
+  M5Cardputer.Display.clear();
+  M5Cardputer.Display.setTextColor(deck.size() > 0 ? DARKGREEN : MAROON);
+  M5Cardputer.Display.setTextDatum(textdatum_t::top_left);
+  M5Cardputer.Display.drawString("Cards Remaining: " + String(deck.size()), 0, 0);
+  M5Cardputer.Display.setTextColor(DARKGREY);
+  M5Cardputer.Display.setTextDatum(textdatum_t::top_right);
+  M5Cardputer.Display.drawString("[D]raw", displayWidth, 0);
+  M5Cardputer.Display.setTextDatum(textdatum_t::top_left);
+
+  if (cardsToShow > 0 && cardsToShow <= drawn.size()) {
+    for (uint8_t i = 1; i <= cardsToShow; i++) {
+      //split into two columns
+      if (i <= MAX_CARDS_TO_SHOW / 2) {
+        M5Cardputer.Display.drawString(displayCard(drawn.at(drawn.size() - i)), 0, (i + 1) * fontHeight);
+      } else if (i <= MAX_CARDS_TO_SHOW) {
+        M5Cardputer.Display.drawString(displayCard(drawn.at(drawn.size() - i)), displayWidth / 2, (i - 3) * fontHeight);
+      }
+    }
+  }
+
+  M5Cardputer.Display.setTextColor(DARKGREY);
+  M5Cardputer.Display.setTextDatum(textdatum_t::bottom_left);
+  String s = allowJokers ? "[J]okers=on" : "[J]okers=off";
+  M5Cardputer.Display.drawString(s, 0, displayHeight);
+  M5Cardputer.Display.setTextDatum(textdatum_t::bottom_center);
+  M5Cardputer.Display.drawString("      [C]lear", displayWidth / 2, displayHeight);
+  M5Cardputer.Display.setTextDatum(textdatum_t::bottom_right);
+  M5Cardputer.Display.drawString("[S]huffle", displayWidth, displayHeight);
+}
+
+void drawCard() {
+  if (!deck.empty()) {
+    // generate a random index, pull that card from the deck and add it to the drawn pile
+    int8_t cardIndex = random(0, deck.size());
+    drawn.push_back(deck.at(cardIndex));
+    deck.erase(deck.begin()+cardIndex);
+    if (cardsToShow < MAX_CARDS_TO_SHOW) { cardsToShow++; }
+    displayDeck();
+  }
+}
+
+String displayCard(String thisCard) {
+  if (thisCard == "Joker-RED") {
+    M5Cardputer.Display.setTextColor(WHITE, RED);
+    return " JOKER ";
+  } else if (thisCard == "Joker-BLACK") {
+    M5Cardputer.Display.setTextColor(BLACK, WHITE);
+    return " JOKER ";
+  } else if (thisCard.endsWith("Clubs") || thisCard.endsWith("Spades")) {
+    M5Cardputer.Display.setTextColor(WHITE);
+  } else if (thisCard.endsWith("Diamonds") || thisCard.endsWith("Hearts")) {
+    M5Cardputer.Display.setTextColor(RED);
+  } else {
+    M5Cardputer.Display.setTextColor(DARKGREEN);
+  }
+  return thisCard;
+}
 
 /* If we have to do the roll, do it, and add the modifier,
  * and set it up for display.
@@ -368,7 +460,7 @@ int findSplitPos(String s) {
 /* If any state has changed, then
  * update the display.
  */
-void handleDisplay() {
+void rollerHandleDisplay() {
   if (!stateChange) {
     return;
   }
@@ -495,17 +587,18 @@ void splashHandleDisplay() {
 
   M5Cardputer.Display.setTextColor(RED);
   M5Cardputer.Display.setTextDatum(textdatum_t::top_center);
-  M5Cardputer.Display.drawString("Savage Worlds Dice Roller", displayWidth / 2, 0 * fontHeight);
+  M5Cardputer.Display.drawString("Savage Worlds Dice Roller", displayWidth / 2, 0);
 
   M5Cardputer.Display.setTextColor(GREEN);
   M5Cardputer.Display.setTextDatum(textdatum_t::top_center);
-  M5Cardputer.Display.drawString("by Douglas P. Fields, Jr.", displayWidth / 2, 3 * fontHeight / 2);
+  M5Cardputer.Display.drawString("by Douglas P. Fields, Jr.", displayWidth / 2, fontHeight);
 
   M5Cardputer.Display.setTextColor(CYAN);
   M5Cardputer.Display.setTextDatum(textdatum_t::top_center);
-  M5Cardputer.Display.drawString("24680 adds a die, odd subs", displayWidth / 2, 3 * fontHeight);
-  M5Cardputer.Display.drawString("[w]ild, [e]xplode, +/- mod", displayWidth / 2, 4 * fontHeight);
-  M5Cardputer.Display.drawString("sp/ret rolls, esc reset, ? help", displayWidth / 2, 5 * fontHeight);
+  M5Cardputer.Display.drawString("24680 adds a die, odd subs", displayWidth / 2, 2 * fontHeight);
+  M5Cardputer.Display.drawString("[w]ild, [e]xplode, +/- mod", displayWidth / 2, 3 * fontHeight);
+  M5Cardputer.Display.drawString("sp/ret rolls, esc reset, ? help", displayWidth / 2, 4 * fontHeight);
+  M5Cardputer.Display.drawString("[c]ombat to show card deck", displayWidth / 2, 5 * fontHeight);
 
   M5Cardputer.Display.setTextColor(YELLOW);
   M5Cardputer.Display.setTextDatum(textdatum_t::bottom_left);
@@ -526,10 +619,36 @@ void splashHandleKeys() {
       currentPage = Page::Roller;
       // Display the Roller page when we first get to it
       firstRun = 1;
+    } else if (isNewlyPressed('c')) {
+      currentPage = Page::DeckofCards;
+      displayDeck();
     }
   }
 }
 
+void deckHandleKeys() {
+  if (M5Cardputer.Keyboard.isChange()) {
+    calculateNewlyPressed();
+
+    if (isNewlyPressed('/') || isNewlyPressed('?')) {
+      // return to splash
+      firstRun = 1;
+      currentPage = Page::Splash;
+    } else if (isNewlyPressed('d')) {
+      drawCard();
+    } else if (isNewlyPressed('c')) {
+      cardsToShow = 0;
+      displayDeck();
+    } else if (isNewlyPressed('s')) {
+      shuffleDeck();
+      displayDeck();
+    } else if (isNewlyPressed('j')) {
+      allowJokers = !allowJokers;
+      shuffleDeck();
+      displayDeck();
+    }
+  }
+}
 
 /* Main Arduino event loop entry.
  */
@@ -543,9 +662,11 @@ void loop() {
     splashHandleDisplay();
     splashHandleKeys();
   } else if (currentPage == Page::Roller) {
-    handleKeys();
+    rollerHandleKeys();
     updateState();
-    handleDisplay();
+    rollerHandleDisplay();
+  } else if (currentPage == Page::DeckofCards) {
+    deckHandleKeys();
   }
 }
 
